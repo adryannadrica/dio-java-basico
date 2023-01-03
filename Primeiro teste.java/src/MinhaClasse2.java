d2fb757d76dfaa799f4304e9c481eb62c7f258bc
adriana <adryannadrica@hotmail.com>
2023-01-03 20:19:59 -0300

    aumentando o string

--- Primeiro teste.java/src/MinhaClasse2.java
+++ Primeiro teste.java/src/MinhaClasse2.java
@@ -1,18 +1,22 @@
 public class MinhaClasse2 {
+    
     public static void main ( String [] args) {
 
 
     String primeiroNome = "Adriana";
-    String segundoNome = "Ribeiro";
+    String segundoNome = "Andrea da Silva ";
+    String terceiroNome = "Ribeiro";
 
-    String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome, terceiroNome);
 
     System.out.println(nomeCompleto) ;
 
-}
-    public static String nomeCompleto ( String primeiroNome, String segundoNome) {
-        return primeiroNome.concat(" ").concat(segundoNome) ;
+ }
+ 
+    public static String nomeCompleto (String primeiroNome,String segundoNome,String terceiroNome) {
+        return primeiroNome.concat(" ").concat(segundoNome).concat(terceiroNome); 
+        }
+        }
 
-    }
+    
 
-}
